914e77c678c987771020dfe22e8d42211c60a4b2
RoninQA <ronierisoncosta@gmail.com>
2022-03-25 18:59:38 -0300

    Aula 05 exercício de encapsulamento

---
+++ src/aula_05/Aula_05.java
@@ -0,0 +1,13 @@
+package aula_05;
+
+public class Aula_05 {
+    public static void main(String[] args) {
+        ControleRemoto c = new ControleRemoto();
+        c.ligar();
+        c.maisVolume();
+        c.play();
+        c.abrirMenu();
+        c.fecharMenu();
+
+    }
+}
