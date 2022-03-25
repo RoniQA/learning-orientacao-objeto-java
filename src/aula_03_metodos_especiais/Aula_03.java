096d7b7a44be8af960c79ce7b31784adf9366a80
RoninQA <ronierisoncosta@gmail.com>
2022-03-25 14:42:06 -0300

    Exercício da aula 03 : métodos especiais. construct, getter, setter

---
+++ src/aula_03_metodos_especiais/Aula_03.java
@@ -0,0 +1,11 @@
+package aula_03_metodos_especiais;
+
+public class Aula_03 {
+    public static void main(String[] args) {
+        Caneta c1 = new Caneta("NIC","Vermelha",0.8f);
+        c1.status();
+
+        Caneta c2 = new Caneta("BIC","Preta",0.5f);
+        c2.status();
+    }
+}
