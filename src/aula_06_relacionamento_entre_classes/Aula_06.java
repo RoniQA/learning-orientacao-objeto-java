f3be37cbfbcd01651aabf003106f53bee5866678
RoninQA <ronierisoncosta@gmail.com>
2022-03-28 16:59:39 -0300

    Aula 06 -alteração dos nomes hehehe

--- src/aula_06_relacionamento_entre_classes/Aula_06.java
+++ src/aula_06_relacionamento_entre_classes/Aula_06.java
@@ -4,17 +4,17 @@ public class Aula_06 {
     public static void main(String[] args) {
         Lutador l[] = new Lutador[6];
 
-        l[0] = new Lutador("Pretty Boy", "França", 31, 1.75f, 68.9f,
+        l[0] = new Lutador("Cadinho", "França", 31, 1.75f, 68.9f,
                             11, 2, 1);
-        l[1] = new Lutador("Putscript", "Brasil", 29, 1.68f, 57.8f,
+        l[1] = new Lutador("Gustavo(Dudu)", "Brasil", 29, 1.68f, 57.8f,
                             14, 2, 1);
-        l[2] = new Lutador("Snapshadow", "EUA", 35, 1.65f, 80.9f,
+        l[2] = new Lutador("Ronin", "EUA", 35, 1.65f, 80.9f,
                             12, 2, 1);
-        l[3] = new Lutador("MickHill", "Brasil", 27, 1.7f, 81.3f,
+        l[3] = new Lutador("Alegria do Xande", "Brasil", 27, 1.7f, 81.3f,
                 10, 2, 4);
-        l[4] = new Lutador("UFOCobol", "Brasil", 37, 1.70f, 119.3f,
+        l[4] = new Lutador("MickHill", "Brasil", 37, 1.70f, 119.3f,
                 5, 4, 3);
-        l[5] = new Lutador("Nerdaart", "EUA", 30, 1.81f, 105.7f,
+        l[5] = new Lutador("Mc Mick", "EUA", 30, 1.81f, 105.7f,
                             12, 2, 4);
 
         Luta EFC01 = new Luta();
